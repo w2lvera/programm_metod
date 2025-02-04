6482acb09b08f247be9b17ba8a964fb240154eb1
w2lvera <w2lvera@gmail.com>
2025-02-04 20:10:43 +0300

    class data && metod

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
 using namespace std;
 
+
+#include <string.h>
+
+
+class Man{
+public:
+    char name[20];
+    int age;
+    int pay;
+    void print(){
+        printf("%s  %d  %d", name, age, pay);
+    }
+};
 int main()
 {
-    cout << "Hello World!" << endl;
+    Man student1;
+    student1.age =18;
+    student1.pay = 0;
+    strcpy(student1.name,"vacy");
+    student1.print();
+   // printf("%s  %d  %d",student1.name, student1.age, student1.pay);
     return 0;
 }
