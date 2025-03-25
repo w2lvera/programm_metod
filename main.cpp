324b80f1da2ed5911a329ac70f857c20342d54df
w2lvera <w2lvera@gmail.com>
2025-03-25 21:52:27 +0300

    sort whith qsort

--- main.cpp
+++ main.cpp
@@ -48,6 +48,11 @@ public:
     static bool compPay(Man& m1,  Man& m2){
         return m1.pay<m2.pay;
     }
+    static int compAgeQsort(const void* a,const void* b){
+        Man* m1 = (Man*)a;
+        Man* m2 = (Man*)b;
+        return m1->age-m2->age;
+    }
 };
 template <class T>
 void printArr(T*a,int n){
@@ -57,21 +62,27 @@ void printArr(T*a,int n){
 bool compInt(int& a, int& b){
     return a<b;
 }
-
+int compIntQsort(const void* a,const void* b){
+    int* ap =(int*)a;
+    int* bp = (int*)b;
+    return *ap - *bp;
+}
 int main()
 {
-    Man gr11[4]= {{"s2",25,5000},{"s1",19,10},{"s0", 21,2305},{"a0", 20,2300}};
-    cout<<"\n gr11 print \n";
-    for(Man x:gr11) cout<<x<<"\n";
+//    Man gr11[4]= {{"s2",25,5000},{"s1",19,10},{"s0", 21,2305},{"a0", 20,2300}};
+//    cout<<"\n gr11 print \n";
+//    for(Man x:gr11) cout<<x<<"\n";
 //    insert(gr11,4,Man::compPay);
-    qs(gr11,0,3,Man::compAge);
+//    qs(gr11,0,3,Man::compAge);
+//    qsort(gr11,4,sizeof(Man),Man::compAgeQsort);
  //   qs(gr11,0,3);
 //   insert(gr11,4);
-    cout<<"\n gr11 print after sort\n";
-    for(Man x:gr11) cout<<x<<"\n";
-//    int arr[8]={42,55,12,44,94,18,6,67};
-//    shell(arr,8,compInt);
-//    qs(arr,0,7,compInt);
-//    printArr(arr,8);
+//    cout<<"\n gr11 print after sort\n";
+//    for(Man x:gr11) cout<<x<<"\n";
+    int arr[8]={42,55,12,44,94,18,6,67};
+    //shell(arr,8,compInt);
+   // qs(arr,0,7,compInt);
+    qsort(arr,8,sizeof(int),compIntQsort);
+    printArr(arr,8);
     return 0;
 }
