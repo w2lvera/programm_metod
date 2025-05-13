aa0dfc624fee8b593a30558a232a7bbfcdb50f26
w2lvera <w2lvera@gmail.com>
2025-05-13 21:29:25 +0300

    tree whith copyconstructor

--- main.cpp
+++ main.cpp
@@ -6,29 +6,7 @@ using namespace std;
 #include <string.h>
 #include <string>;
 
-class Man{
-public:
-    //class data
-    char name[20]="";
-    int age=0;
-    int pay=0;
-    //constructor
-    Man(){}//default constructor
-    Man(const char* n, int a,int p){//constructor with parameters
-        strcpy(name,n);
-        age = a;
-        pay = p;
-    }
-    Man(Man& m){
-        strcpy(name,m.name);
-        age = m.age;
-        pay = m.pay;
-    }
-    //class metod
-    void print(){
-        printf("%10s   %4d  %d\n", name, age, pay);
-    }
-};
+
 class FrequencyName{
 public:
     string name;
@@ -79,6 +57,13 @@ private:
             LRootR(root->right,h+1,os);
         }
     }
+    void RootLR(T* root){
+        if(root){
+            addEll(this->root,root->info);
+            RootLR(root->left);
+            RootLR(root->right);
+        }
+    }
     void LRootR(T* root,G*& arr){
         if(root){
             LRootR(root->left,arr);
@@ -86,7 +71,7 @@ private:
             LRootR(root->right,arr);
         }
     }
-    void RRootL(T* root,int*& arr){
+    void RRootL(T* root,G*& arr){
         if(root){
             RRootL(root->right,arr);
             *arr=root->info; arr++;
@@ -101,6 +86,10 @@ private:
     }
 
 public:
+    Tree(){}
+    Tree(const Tree& t){
+        RootLR(t.root);
+    }
     void add(G x){
         addEll(root,x);
     }
@@ -128,10 +117,11 @@ int main()
 {
     int arr[]={8,9,11,15,19,20,21,7,3,2,1,5,6,4,13,14,10,12,17,16,18};
     int arr1[21];
-   // int* p  = arr1;
     Tree<int> tree;
     for(int x:arr)tree.add(x);
     cout<<tree<<"\n";
+    Tree<int> treeCopy(tree);
+    cout<<"\n tree copy\n"<<treeCopy;
     tree.sortIncreasing(arr1);
     for(int x:arr1) cout<<x<<" ";
     tree.sortDescending(arr1);
@@ -143,24 +133,6 @@ int main()
         fn.addFn(name);
     cout<<"\n names\n";
     cout<<fn;
-//    Man student1;//student1 - object or class instance
-//    student1.age =18;
-//    student1.pay = 0;
-//    strcpy(student1.name,"Vacy");
-//    Man student2("Koly",20,2300);
-//    Man student3("Grisha",19,2300);
-//    student1.print();
-//    student2.print();
-//    student3.print();
 
-//    //initialization on declaration
-//    printf("\n initialization on declaration\n");
-//    Man student4={"Masha",15,0};
-//    Man gr[4]= {student1,student2,student3,student4};
-//    for(Man x:gr)x.print();
-//    Man gr11[3]= {{"s1",15,0},{"s2",19,0},{"s3", 20,2300}};
-//    printf("\n gr11 print \n");
-//    for(Man x:gr11) x.print();
-//   // printf("%s  %d  %d",student1.name, student1.age, student1.pay);
     return 0;
 }
